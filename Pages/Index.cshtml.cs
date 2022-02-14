6ba65b93cee1f0f5ade53250c3b9177f40ef145b
Starway23 <plamentrakiiski2@abv.bg>
2022-02-14 21:19:37 +0200

    Categories are now displayed on front page.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RedForums.Data.Services;
+using RedForums.Models;
 
 namespace RedForums.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ICategoriesService categoriesService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public ICollection<CategoryViewModel> Categories { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, ICategoriesService categoriesService)
         {
             _logger = logger;
+            this.categoriesService = categoriesService;
+            Categories = new HashSet<CategoryViewModel>();
         }
 
         public void OnGet()
         {
-
+            Categories = categoriesService.GetAll<CategoryViewModel>().ToHashSet();
         }
     }
 }

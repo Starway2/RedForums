a19f45e321c80c076703ef5f0265333ffdb600db
Starway23 <plamentrakiiski2@abv.bg>
2022-03-12 13:11:24 +0200

    Added tinyMCE to posts and comments

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿tinymce.init({
+    selector: 'textarea',
+    plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    toolbar_mode: 'floating',
+});
